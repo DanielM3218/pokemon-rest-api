3e2c0a189f9773267f030a699182f6cf75f7e0a3
DanielM3218 <daniel.inon.melki@gmail.com>
2025-04-08 14:20:01 +0200

    this is branch main

---
+++ src/main/kotlin/com/example/pokemon_api_final/PokemonApiFinalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pokemon_api_final
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokemonApiFinalApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokemonApiFinalApplication>(*args)
+}
