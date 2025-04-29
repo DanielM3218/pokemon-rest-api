2b5a101af52b7cbdf29519352a08f709365b6565
DanielM3218 <daniel.inon.melki@gmail.com>
2025-04-29 13:36:39 +0200

    deleted comments and unused imports

--- src/main/kotlin/com/example/pokemon_api_final/config/WebConfig.kt
+++ src/main/kotlin/com/example/pokemon_api_final/config/WebConfig.kt
@@ -1,6 +1,5 @@
 package com.example.pokemon_api_final.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -8,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/api/**")  // Allow cross-origin requests for all API endpoints
-            .allowedOrigins("http://localhost:5173")  // Allow only your React app
-            .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow the methods you need
-            .allowedHeaders("*")  // Allow all headers (optional)
-            .allowCredentials(true)  // Allow credentials (cookies, etc.)
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
